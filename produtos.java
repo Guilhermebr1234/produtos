7f5e25ed9b0dc7d9bb97cb9237212b3249cfd6a2
Guilhermebr1234 <168814221+Guilhermebr1234@users.noreply.github.com>
2024-08-13 13:51:55 -0300

    Create produtos.java

---
+++ produtos.java
@@ -0,0 +1,52 @@
+
+ 
+
+abstract class produtos {
+
+ public static void main(String[] args) {
+    
+
+
+
+
+
+
+   
+   void cadastrar() {
+
+
+
+
+   }
+ 
+
+void selecionar(){
+
+
+}
+
+    
+
+void alterar(){
+
+
+
+
+}
+
+void remover(){
+
+
+
+
+
+}
+
+
+}
+    
+            
+        
+    }
+    
+
